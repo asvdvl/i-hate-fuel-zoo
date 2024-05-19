227d3dce973fb7c3833201a352917f1b3f512373
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-05-19 04:35:13 +0300

    adde fluid converter as different assembler

--- data.lua
+++ data.lua
@@ -1,18 +1,26 @@
 local category = "fuel-unification"
 local assembler = "fuel-unifier"
-local fuel_names = {fluid = "heat-transfer-fluid", item = "heat-conducting-fuel"}
-local fuel_value = "10MJ"
+local fuel_params = {
+    fluid = {
+        name = "heat-transfer-fluid",
+        value = "1MJ"
+    },
+    item = {
+        name = "heat-conducting-fuel",
+        value = "10MJ"
+    }
+}
 local tint = {1, 0.5, 0.5}
 
 data:extend({
     {
         type = "fluid",
-        name = fuel_names.fluid,
+        name = fuel_params.fluid.name,
         icon = "__i-hate-fuel-zoo__/graphics/icons/heat-transfer-fluid.png",
         icon_size = 64,
         icon_mipmaps = 4,
-        heat_capacity = "1MJ",
-        fuel_value = fuel_value,
+        heat_capacity = "1kJ",
+        fuel_value = fuel_params.fluid.value,
         gas_temperature = 15,
         base_color = tint,
         flow_color = tint,
@@ -24,7 +32,7 @@ data:extend({
 if mods["aai-industry"] then
     category = "fuel-processing"
     assembler = "fuel-processor"
-    fuel_names.item = "processed-fuel"
+    fuel_params.item.name = "processed-fuel"
 else
     data:extend({
         {
@@ -39,23 +47,15 @@ else
     })
 
     --assembler
-    local input_fb = {}
     local assembler_proto = table.deepcopy(data.raw["assembling-machine"]["chemical-plant"])
     assembler_proto.name = assembler
     assembler_proto.module_specification.module_slots = 0
     assembler_proto.crafting_categories = {category}
-    assembler_proto.energy_usage = "1MW"
-    for key, fb in pairs(assembler_proto.fluid_boxes) do
-        if fb.production_type == "input" then
-            input_fb = fb
-            assembler_proto.fluid_boxes[key] = nil
-            break
-        end
-    end
+    assembler_proto.energy_usage = fuel_params.fluid.value
+
     assembler_proto.energy_source = {
-        type = "fluid",
-        fluid_box = input_fb,
-        scale_fluid_usage = true
+        type = "burner",
+        fuel_inventory_size = 1,
     }
 
     assembler_proto.icons = {{
@@ -73,15 +73,16 @@ else
         end
     end
 
+    --items
     local assembler_item_proto = table.deepcopy(data.raw["item"]["chemical-plant"])
     assembler_item_proto.subgroup = "smelting-machine"
+    assembler_item_proto.name = assembler
     assembler_item_proto.place_result = assembler
     assembler_item_proto.icons = assembler_proto.icons
 
-    --items
     local fuel_proto = table.deepcopy(data.raw["item"]["solid-fuel"])
-    fuel_proto.name = fuel_names.item
-    fuel_proto.fuel_value = fuel_value
+    fuel_proto.name = fuel_params.item.name
+    fuel_proto.fuel_value = fuel_params.item.value
 
     fuel_proto.icons = {{
         icon = fuel_proto.icon,
@@ -100,19 +101,55 @@ else
     end
     data:extend(protos)
 
-    --recipe
     data:extend({
         {
             type = "recipe",
-            name = fuel_names.fluid,
+            name = fuel_params.item.name,
             enabled = true,
             energy_required = 1,
             ingredients = {},
             category = category,
-            results = {{type = 'fluid', name = fuel_names.fluid, amount = 1}},
+            results = {{type = 'item', name = fuel_params.item.name, amount = 1}},
         }
     })
 end
 
+--recipe
+data:extend({
+    {
+        type = "recipe",
+        name = fuel_params.fluid.name,
+        enabled = true,
+        energy_required = 1,
+        ingredients = {},
+        category = category,
+        results = {{type = 'fluid', name = fuel_params.fluid.name, amount = 1}},
+    }
+})
+
+--assembler
+local input_fb = {}
+local asm_fluid_input = assembler.."-fluid-input"
+local assembler_proto = table.deepcopy(data.raw["assembling-machine"][assembler])
+assembler_proto.name = asm_fluid_input
+assembler_proto.energy_usage = fuel_params.fluid.value
+
+for key, fb in pairs(assembler_proto.fluid_boxes) do
+    if fb.production_type == "input" then
+        input_fb = fb
+        assembler_proto.fluid_boxes[key] = nil
+        break
+    end
+end
+assembler_proto.energy_source = {
+    type = "fluid",
+    fluid_box = input_fb,
+    scale_fluid_usage = true
+}
+
+--items
+local assembler_item_proto = table.deepcopy(data.raw["item"][assembler])
+assembler_item_proto.name = asm_fluid_input
+assembler_item_proto.place_result = asm_fluid_input
 
 
